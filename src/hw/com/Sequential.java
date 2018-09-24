dcc2fc12254be004fc380484486b37a9275ebb92
cutebaby0630 <lovemath0630@gmail.com>
2018-09-24 14:46:11 +0800

    循序搜尋法

---
+++ src/hw/com/Sequential.java
@@ -0,0 +1,26 @@
+package hw.com;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Sequential {
+
+	public static void main(String[] args) {
+		String M;
+		BufferedReader keyin = BufferedReader(new InputStreamReader(System.in));
+		int data[] = new int [100];
+		int i,j,find;
+		int val = 0;
+		for(i=0;i<80;i++)
+			data[i] = (((int)(Math.random()*150))%150+1);
+						
+		
+		
+	}
+
+	private static BufferedReader BufferedReader(InputStreamReader inputStreamReader) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
